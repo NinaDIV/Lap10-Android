9fdd6ff04732b45b686770d7ad0146ccf9acbfc8
NinaDEV <168221598+NinaDIV@users.noreply.github.com>
2025-05-18 13:58:31 -0500

    Delete codelab-bookshelf-app-android-kotlin-compose-end directory

--- codelab-bookshelf-app-android-kotlin-compose-end/app/src/main/java/com/example/bookshelf/AppDestinations.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bookshelf
-
-enum class AppDestinations(val title: String) {
-    MenuScreen(title = "Menú"),
-    QueryScreen(title = "Estante de Libros de Google"),
-    FavoriteScreen(title = "Mis Libros Favoritos"),
-    DetailScreen(title = "Libro: ")
-}
